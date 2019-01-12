f5b7f38aad4406e99a7a66b51270f32a30c7b46b
unknown <479anand@gmail.com>
2019-01-12 19:05:07 +0530

    Initial Commit

---
+++ sample-program/Numbers/test/devops/numbers/NumberWordsTests.java
@@ -0,0 +1,55 @@
+package devops.numbers;
+
+import org.junit.*;
+
+import devops.numbers.NumberWords;
+
+public class NumberWordsTests {
+	private NumberWords numberWords ;
+	
+	@Before
+	public void setup() {
+		numberWords = new NumberWords() ;
+	}
+
+	@Test
+	public void numberOutOfRangeReturnsError1() {
+		Assert.assertEquals( "Error", "Number out of range", numberWords.toWords( -1 ) ) ;
+	}
+	
+	@Test
+	public void numberOutOfRangeReturnsError2() {
+		Assert.assertEquals( "Error", "Number out of range", numberWords.toWords( 1000 ) ) ;
+	}
+	
+	
+	@Test
+	public void testPositiveScenario1() {
+		Assert.assertEquals("Error","Eighteen", numberWords.toWords( 18 ) ) ;
+	}
+	
+	@Test
+	public void testPositiveScenario2() {
+		Assert.assertEquals( "Error", "Forty", numberWords.toWords( 40) ) ;
+	}
+	
+	@Test
+	public void testPositiveScenario3() {
+		Assert.assertEquals("Error","Eighty Four", numberWords.toWords( 84 ) ) ;
+	}
+	
+	@Test
+	public void testPositiveScenario4() {
+		Assert.assertEquals("Error","Five Hundred Twelve", numberWords.toWords( 512 ) ) ;
+	}
+
+	@Test
+	public void testPositiveScenario5() {
+		Assert.assertEquals( "Error", "Four Hundred", numberWords.toWords( 400) ) ;
+	}
+	
+	@Test
+	public void testPositiveScenario6() {
+		Assert.assertEquals( "Error", "", numberWords.toWords( 0 ) ) ;
+	}
+}
